598e52a8ccba03ee1c688874a538c57511432723
brunofluna <bruno@brunoluna.com.br>
2024-09-25 15:41:32 -0300

    versão 2.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pessoa import *
 from manipulador import *
 
 if __name__ == '__main__':
-    # instancia dos objetos
+    # instanciando os objetos
     p = Pessoa(0,'','','','')
     m = Manipulador()
 
@@ -14,6 +14,8 @@ if __name__ == '__main__':
         print('1 - Criar novo arquivo JSON.')
         print('2 - Abrir e ler arquivo JSON.')
         print('3 - Salvar novo usuário JSON.')
+        print('4 - Alterar dados do usuário.')
+        print('5 - Deletar usuário.')
         print('0 - Sair do programa.')
 
         opcao = input("Digite a opção desejada: ")
@@ -44,16 +46,57 @@ if __name__ == '__main__':
                     continue
             case '3':
                 try:
-                    usuario = {}
-                    campos = ('nome', 'cpf','email', 'profissao')
+                    #usuario = {}
+                    #campos = ('nome', 'cpf','email', 'profissao')
+                    #print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
+                    #usuario['codigo'] = len(usuario)
+                    #for campo in campos:
+                    #    usuario[campo] = input(f'Digite o campo {campo.capitalize()}: ')
+                    #usuarios.append(usuario)
+                    #print(m.salvar_dados(usuario, abrir_arquivo))
                     print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
-                    usuario['codigo'] = len(usuario)
-                    for campo in campos:
-                        usuario[campo] = input(f'Digite o campo {campo.capitalize()}: ')
+                    p.codigo = len(usuarios)
+                    p.nome = input('Informe o nome: ')
+                    p.cpf = input('Informe o cpf: ')
+                    p.email = input('Informe o email: ')
+                    p.profissao = input('Informe a profissao: ')
+                    usuario = dict(codigo=p.codigo, nome=p.nome, cpf=p.cpf, email=p.email, profissao=p.profissao) #dict outra forma de criar um dicionário
                     usuarios.append(usuario)
-                    print(m.salvar_dados(usuario, abrir_arquivo))
+                    print(m.salvar_dados(usuarios, abrir_arquivo))
                 except Exception as e:
                     print(f'Não foi possível realizar a operação. {e}.')
+                finally:
+                    continue
+            case '4':
+                try:
+                    print(f'Arquivo aberto: {abrir_arquivo}.json. \n')
+                    codigo = int(input('Digite o código do usuário que deseja alterar os dados: '))
+                    for campo in usuarios[codigo]:
+                        print(f'Valor atual do campo {campo}: {usuarios[codigo].get(campo)}')
+                        novo_dado = input(f'Digite o novo dado do campo {campo} ou aperte "Enter" caso deseje manter o mesmo valor: ')
+                        if novo_dado:
+                            usuarios[codigo][campo] = novo_dado
+                        else:
+                            ...
+                    print(m.salvar_dados(usuarios, abrir_arquivo))
+                except Exception as e:
+                    print(f'Não foi possível alterar os dados. {e}.')
+                finally:
+                    continue
+            case '5':
+                try:
+                    print(f'Arquivo aberto: {abrir_arquivo}.json.\n')
+                    codigo = int(input('Digite o código do usuário que deseja deletar: '))
+                    nome_deletado = usuarios[codigo]['nome']
+                    confirmacao = input(f'Deseja deletar o usuário {nome_deletado}? \nDigite "SIM" para confirmar: ').upper()
+                    if confirmacao == "SIM":
+                        del(usuarios[codigo])
+                        print(m.salvar_dados(usuarios, abrir_arquivo))
+                        print(f'Usuário {nome_deletado} deletado com sucesso.')
+                    else:
+                        print(f'Usuário {nome_deletado} não foi excluído.')
+                except Exception as e:
+                    print(f"Não foi possível deletar o arquivo. {e}.")
                 finally:
                     continue
             case _:
--- manipulador.py
+++ manipulador.py
@@ -14,7 +14,6 @@ class Manipulador:
                     'Profissao': 'Administrador'
                 }
             ]
-
             # serializando objeto python como json
             json_dados = json.dumps(usuarios)
             with open(f"{nome_arquivo}.json", 'w', encoding='utf-8') as f:
@@ -22,11 +21,13 @@ class Manipulador:
             return f'{nome_arquivo}.json criado com sucesso!'
         except Exception as e:
             return f'Não foi possível criar o arquivo. {e}.'
+        
     def abrir_arquivo(self, nome_arquivo):
         # transformar ou seja desserializando objeto json para pyrhon
         with open(f'{nome_arquivo}.json', 'r', encoding='utf-8') as f:
             dados = json.load(f)
             return dados
+        
     def salvar_dados(self, usuarios, nome_arquivo):
         try:
             with open(f'{nome_arquivo}.json', 'w', encoding='utf-8') as f:
